ac67a03abe361a5b6aad767c7367cfaddf1405ab
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-13 10:21:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def dictionary
+  words_to_be_substituted => {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    be: "be",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+  

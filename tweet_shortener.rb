b85e0f35092216bbb4134dbe6f646754bc110159
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-13 10:35:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,16 +2,19 @@ require 'pry'
 
 def dictionary
   words_to_be_substituted = {
-    hello: "hi",
-    to: "2",
-    two: "2",
-    too: "2",
-    be: "be",
-    you: "u",
-    at: "@",
-    and: "&"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "be",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
 
 def word_substituter(tweet)
-  
+  tweet_ary = tweet.split(", ")
+  tweet_ary.each do |x|
+    if tweet_ary.include?(dictionary)
+      tweet_ary.gsub(x, dictionary)
